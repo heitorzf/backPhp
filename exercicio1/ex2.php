9727e2cc91eca73651ea678878b2c6f3534706da
Heitor Zanini Formanski <heitor.246616@alunosatc.edu.br>
2025-02-20 17:15:28 -0300

    arrumado

--- exercicio1/ex2.php
+++ exercicio1/ex2.php
@@ -1,8 +1,9 @@
 <?php 
-    $nome = $_POST['nome'];
+    $nome2 = $_POST['nome2'];
     $nota1 = $_POST['nota1'];
     $nota2 = $_POST['nota2'];
-    echo ($nota1 + $nota2) / 2;
+    $media = ($nota1 + $nota2) / 2;
+    echo "Aluno: ".$nome2."Media final: ".$media;
 
 
 ?>
